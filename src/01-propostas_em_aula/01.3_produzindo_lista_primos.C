6b2494642059f8e2a0768d914a78645136fee10e
Isaque <isaquetargino144@gmail.com>
2025-03-23 20:51:29 -0400

    Create 01.3_produzindo_lista_primos.C

---
+++ src/01-propostas_em_aula/01.3_produzindo_lista_primos.C
@@ -0,0 +1,4 @@
+// Usando os axiomas conhecidos, faça com que o computador produza uma lista de primos sequencialmente
+// - Considere N como quantidade de primos a serem construídos a partir de 2
+//   N = 3 -> 2, 3 e 5
+
